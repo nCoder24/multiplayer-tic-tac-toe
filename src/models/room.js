020e93e9d5397ea40fbde1796a9e801ffe4c4134
Utsab Bera <nCoder24@users.noreply.github.com>
2023-08-26 19:03:01 +0530

    add functionality to add and check if full in room

--- src/models/room.js
+++ src/models/room.js
@@ -7,6 +7,14 @@ class Room {
     this.#members = [];
   }
 
+  isFull() {
+    return this.#members.length >= this.#size;
+  }
+
+  add(user) {
+    this.#members.push(user);
+  }
+
   details() {
     return { members: [...this.#members] };
   }
---
+++ test/models/room-test.js
@@ -0,0 +1,43 @@
+const assert = require("node:assert");
+const { describe, it, beforeEach } = require("node:test");
+const Room = require("../../src/models/room");
+
+describe("Room", () => {
+  const user = "user1";
+  const otherUser = "user1";
+  let room;
+
+  beforeEach(() => {
+    room = new Room(2);
+  });
+
+  describe("add", () => {
+    it("should add a member for the first time", () => {
+      room.add(user);
+      assert.deepStrictEqual(room.details(), { members: [user] });
+    });
+
+    it("should add a member after the existing members", () => {
+      room.add(user);
+      room.add(otherUser);
+      assert.deepStrictEqual(room.details(), { members: [user, otherUser] });
+    });
+  });
+
+  describe("isFull", () => {
+    it("should be false if room is empty", () => {
+      assert.ok(!room.isFull());
+    });
+
+    it("should be false if number of members is less than the size", () => {
+      room.add(user);
+      assert.ok(!room.isFull());
+    });
+
+    it("should be true if number of members hit the room size", () => {
+      room.add(user);
+      room.add(otherUser);
+      assert.ok(room.isFull());
+    });
+  });
+});
