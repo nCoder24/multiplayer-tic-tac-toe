dca9e34c63ea1b872ed6a69ccf096b731597a459
Utsab Bera <nCoder24@users.noreply.github.com>
2023-08-26 22:29:31 +0530

    add json and cookie parser

--- src/routers/app.js
+++ src/routers/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const createApp = (gameRouter) => {
   const app = express();
+
+  app.use(express.json());
+  app.use(cookieParser());
+
   app.use(gameRouter);
-  
+
   return app;
 };
 
